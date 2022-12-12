2da1d190e9220fa384cf3786f38daa5273d97ab5
graziacrp <graziacarpineta25@gmail.com>
2022-12-12 16:27:05 +0100

    Aggiungere i file di progetto.

---
+++ ListaIndirizzi/Indirizzo.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaIndirizzi
+{
+    internal class Indirizzo
+    {
+
+        private string nome;
+        private string cognome;
+        private string via;
+        private string città;
+        private string provincia;
+        private string codicepostale;
+
+        public Indirizzo(string nome, string cognome, string via, string città, string provincia, string codicepostale)
+        {
+            this.nome=nome:
+                this.cognome = cognome;
+            this.via = via;
+            this.città = città;
+            this.provincia = provincia;
+            this.codicepostale = codicepostale;
+        }
+
+        public string GetNome()
+        {
+            return nome;
+        }
+
+        public string GetCognome()
+        {
+            return cognome;
+        }
+
+        public string GetVia()
+        {
+            return via;
+        }
+
+        public string GetCittà()
+        {
+            return città;
+        }
+
+        public string GetProvincia()
+        {
+            return provincia;
+
+        }
+
+        public string GetCodicePostale()
+        {
+            return codicepostale;
+        }
+
+        public override string ToString()
+        {
+            return "Nome: " + this.nome + "\tCognome: " + this.cognome + "\tVia: " + this.via + "\tCittà" + this.città + "\tProvincia" + this.provincia + "\tCodicePostale" + this.codicepostale;
+        }
+
+    }
+}
+
---
+++ ListaIndirizzi/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+List<Indirizzo> listaIndirizzi = new List<Indirizzo>;
+
+try
+{
+    //Apro canale di lettura con il file
+    StreamReader file = File.OpenText("addresses.csv");
+
+
+
+
+
+    StreamReader file = File.OpenText("\"C:\\Users\\ALESSANDRA\\Desktop\\Esercizi .NET\\ListaIndirizzi\\addresses.csv\"");
+
+
+
+
+
+    //Controllo che il file non sia finito
+    while (!file.EndOfStream)
+    {
+        //Leggo il contenuto
+        string addressLine = file.ReadLine();
+
+        string[] informazioniIndirizzo = addressLine.Split(" , ");
+
+        string nome = informazioniIndirizzo[0];
+        string cognome = informazioniIndirizzo[1];
+        string via = informazioniIndirizzo[2];
+        string città = informazioniIndirizzo[3];
+        string provincia = informazioniIndirizzo[4];
+        string codicepostale = informazioniIndirizzo[5];
+
+        Indirizzo indirizzoEstratto = new Indirizzo(nome, cognome, via, città, provincia, codicepostale);
+        listaIndirizzi.Add(indirizzoEstratto);
+        Console.WriteLine(addressLine);
+    }
+
+    file.Close();
+}
+
+catch (FileNotFoundException e)
+{
+    Console.WriteLine("File not found");
+
+
+    foreach (Indirizzo indirizzo in listaIndirizzi)
+    {
+        Console.WriteLine(indirizzo);
+    }
+
+}
